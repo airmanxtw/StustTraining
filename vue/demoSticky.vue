c429ad48b5e70d4527a85a1cee82f8338d67d04b
airmanxtw <airmanx@stust.edu.tw>
2022-11-17 00:08:06 +0800

    add sticky

---
+++ vue/demoSticky.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-container>
+      <div v-for="(d,index) in data" :key="index">
+        {{d}}
+      </div>
+      <div class="sticky">
+        <v-btn color="primary" class="mr-2">this is Btn</v-btn>
+        <v-btn color="secondary">this is Btn</v-btn>
+      </div>
+    </v-container>
+  </template>
+  
+  <script setup>
+  import { ref, onMounted } from "vue";
+  let data = ref([]);
+  onMounted(() => {
+    for (let i = 0; i < 100; i++) data.value.push(`this is line no ${i}`);
+  });
+  </script>
+  <style scoped>
+  .sticky {
+    position: -webkit-sticky; /* Safari */
+    position: sticky;
+    bottom: 0;
+    text-align: right;
+    padding-bottom: 10px;
+  }
+  </style>
+  
