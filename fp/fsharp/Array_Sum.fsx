e297719f2b0abafc7401ad2695a31c85211a2497
airmanxtw <airmanx@stust.edu.tw>
2026-05-29 10:57:15 +0800

    新增 Array_Sum.fsx 檔案，實作陣列元素求和及相關函數示範

--- fp/fsharp/Array_Sum.fsx
+++ fp/fsharp/Array_Sum.fsx
@@ -1,5 +1,7 @@
 // Summing the elements of an array
+#r "nuget:FSharpPlus"
 
+open FSharpPlus
 let rec sum1 data =
     match data with
     | [] -> 0
@@ -13,3 +15,11 @@ let rec sum2 data =
 
 printf "The sum of [1; 2; 3] is %d\n" (sum1 [ 1; 2; 3 ])
 printf "The sum of [1; 2; 3] is %d\n" (sum2 [ 1; 2; 3 ])
+
+
+[1;2;3] |> map (fun i -> i*2)
+|> printfn "The double of [1; 2; 3] is %A"
+
+[1;2;3] |> bind (fun i -> [i; i*2])
+|> printfn "The bind of [1; 2; 3] is %A"
+
