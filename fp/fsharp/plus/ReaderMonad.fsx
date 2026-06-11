e9b69399a9cedb29f6769f23941f1e9cd5f55271
airmanxtw <airmanx@stust.edu.tw>
2026-06-11 10:19:27 +0800

    新增 ReaderMonad.fsx 中的 r0 和 r1' 函數，改進錯誤處理邏輯

--- fp/fsharp/plus/ReaderMonad.fsx
+++ fp/fsharp/plus/ReaderMonad.fsx
@@ -4,7 +4,27 @@ open FSharpPlus
 open FSharpPlus.Data
 open FSharpPlus.Lens
 
-let r1 x = Reader(fun (env: int) -> Ok(x + env))
+let e0:Result<int,string> = Error "Negative value"
+let r0 v = if v > 0 then Ok v else Error "Negative value"
+
+let r1 x = Reader(fun (env: int) -> r0 x)
+
+let r1' (x:int) = r1 10 |> Reader.bind (fun r ->
+    match r with
+    | Ok v -> r1 v
+    | Error e -> 
+        let re = Reader(fun _ -> Error e)
+        re
+)
+
+let  t1 = Reader.Return<Result<int,string>,int> (Ok 10)
+let r1'' (x:int) = monad{
+    let! r = r1 10
+    match r with
+    | Ok v -> return! r1 v
+    | Error e -> return! Reader.Return<Result<int,string>,int> (Error e)
+}
+
 
 let result =
     monad {
@@ -23,6 +43,15 @@ let result =
     |> function
         | Ok v -> v
         | Error e -> 0
+
+let result2 = monad {
+    let! x = r1 5
+    let! y = 
+        Result.bimap (fun (e:string) -> Reader(fun _ -> Error e)) (fun v -> r1 v)  x
+
+    
+    
+}
 
 
 printfn "%A" result
