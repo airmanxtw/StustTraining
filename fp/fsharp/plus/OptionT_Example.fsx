62b2b375e7cbb92f9feb4967bd3d054bb39ddc91
airmanxtw <airmanx@stust.edu.tw>
2026-06-14 18:43:18 +0800

    新增 result2 函數以擴展 OptionT 的示範，並調整 result 函數的結構

--- fp/fsharp/plus/OptionT_Example.fsx
+++ fp/fsharp/plus/OptionT_Example.fsx
@@ -7,20 +7,45 @@ open FSharpPlus.Lens
 module OptionTExample =
     let add a b = Ok(Some(a + b))
 
+    let add2 a b = Ok(Ok(Some(a + b)))
 
-    let result() =
+
+    let result () =
         let t = OptionT(add 5 10)
 
         let t2 =
             monad {
                 let! x = t
+
                 return x + 1
             }
 
         t2
 
-    OptionT.run (result())
+    let result2 () =
+        monad {
+            let! x = add 1 2
+            let! y = add 3 4
+
+
+            let z =
+                monad {
+                    let! a = x
+                    let! b = y
+                    return a + b
+                }
+
+            z
+        }
+
+    OptionT.run (result ())
     |> function
         | Ok(Some v) -> printfn "Result: %A" v
         | Ok None -> printfn "Result: None"
+        | Error e -> printfn "Error: %A" e
+
+    result2 ()
+    |> function
+        | Ok(Some v) -> printfn "Result2: %A" v
+        | Ok None -> printfn "Result2: None"
         | Error e -> printfn "Error: %A" e
