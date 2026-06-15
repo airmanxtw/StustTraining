d4e1bf3c042fc2a6d3315c255fb860580beaab61
airmanxtw <airmanx@stust.edu.tw>
2026-06-15 22:22:40 +0800

    新增 factorial 函數並更新 f1Result 的計算邏輯

--- fp/fsharp/plus/Cont.fsx
+++ fp/fsharp/plus/Cont.fsx
@@ -7,6 +7,11 @@ open FSharpPlus.Lens
 let f1 = Cont(fun (c: int -> int) -> c 10)
 let f2 = Cont(fun (c: int -> int) -> c 20)
 
-let f1Result = Cont.run f1 id
+let factorial n =
+    Cont(fun (f: int -> int) -> if n <= 1 then 1 else n * f (n - 1))
+
+
+
+let f1Result = Cont.run (factorial 4)
 
 printfn "%A" f1Result
