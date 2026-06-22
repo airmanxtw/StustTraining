c37867249352ed1e2f36ca1878f063f8af47119e
airmanxtw <airmanx@stust.edu.tw>
2026-06-22 14:18:06 +0800

    feat: 更新 test2 的邏輯，使用 callCC 來處理 factorial 計算

--- fp/fsharp/plus/Cont.fsx
+++ fp/fsharp/plus/Cont.fsx
@@ -10,10 +10,15 @@ let f2 = Cont(fun (c: int -> int) -> c 20)
 let factorial n =
     Cont(fun (f: int -> int) -> if n <= 1 then 1 else n * f (n - 1))
 
-let test2 = factorial 5 |> Cont.bind (fun x -> factorial x)
-
-
-
+let test2 = 
+    Cont.callCC(fun k -> 
+        factorial 5
+        |> Cont.bind(fun x -> 
+            if x>100 then k x else Cont.Return<int, int> (x-2)
+        )  
+        |> Cont.map(fun x -> x-1)  
+    )
+    
 let f1Result = Cont.run (test2) id
 
-printfn "Factorial of 4 is: %d" f1Result
+printfn "Factorial  is: %d" f1Result
