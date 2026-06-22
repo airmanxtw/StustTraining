108127d622461d62709d8055766e0d4d45cf5fb6
airmanxtw <airmanx@stust.edu.tw>
2026-06-22 11:57:45 +0800

    feat: 更新 factorial 測試邏輯，新增 test2 以計算 factorial 5 的結果

--- fp/fsharp/plus/Cont.fsx
+++ fp/fsharp/plus/Cont.fsx
@@ -10,8 +10,10 @@ let f2 = Cont(fun (c: int -> int) -> c 20)
 let factorial n =
     Cont(fun (f: int -> int) -> if n <= 1 then 1 else n * f (n - 1))
 
+let test2 = factorial 5 |> Cont.bind (fun x -> factorial x)
 
 
-let f1Result = Cont.run (factorial 4)
 
-printfn "%A" f1Result
+let f1Result = Cont.run (test2) id
+
+printfn "Factorial of 4 is: %d" f1Result
