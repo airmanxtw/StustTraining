1f1fd08ad611e33e5f2b4bf2c0f2d3ccf1543bcf
airmanxtw <airmanx@stust.edu.tw>
2026-06-25 00:24:12 +0800

    feat: 新增 Math_Applicative.fsx 檔案，實作基本數學運算功能

---
+++ fp/fsharp/plus/Math_Applicative.fsx
@@ -0,0 +1,34 @@
+#r "nuget:FSharpPlus"
+#r "nuget:MathNet.Numerics"
+
+open FSharpPlus
+open FSharpPlus.Data
+open FSharpPlus.Math.Applicative
+
+open MathNet.Numerics.LinearAlgebra.Double
+
+
+let a i = i + 1
+let b i = i + 2
+
+let c = b .+. a
+
+printfn "c: %A" (c 1)
+
+
+
+
+
+
+
+let add (a: int[]) (b: int[]) = a .+. b
+
+//printfn "Add: %A" (add [| 1; 2; 3 |] [| 4; 5; 6 |])
+
+
+let v = Vector.Build.DenseOfArray [| 4; 1 |]
+let w = Vector.Build.DenseOfArray [| 2; -1 |]
+
+let z = 2.0 * v - 3.0 * w
+
+printfn "2v - 3w: %A" z
