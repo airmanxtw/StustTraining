4b9e098c91151fb85c2f275a03275c9a86aa984d
airmanxtw <airmanx@stust.edu.tw>
2026-07-11 22:42:00 +0800

    feat: 在 Applicative.fsx 中新增數據處理示例，展示如何使用函數組合

--- fp/fsharp/plus/Abstractions/Applicative.fsx
+++ fp/fsharp/plus/Abstractions/Applicative.fsx
@@ -1,4 +1,5 @@
 #r "nuget:FSharpPlus"
+
 open FSharpPlus
 open FSharpPlus.Data
 open FSharpPlus.Lens
@@ -7,7 +8,12 @@ let r1 = result 10 |> Async.map (fun x -> x * 2)
 
 let r2 = result 10 |> List.map (fun x -> x * 2)
 
+let data1 = [ 1; 2; 3 ]
+let data2 = [ 4; 5; 6; 7 ]
 
+let result1 = (fun x y -> x + y) <!> data1 <*> data2
 
+printfn "Result1: %A" result1
 
-
+let result2 = (fun x y -> x + y) <!> data1 <.> data2
+printfn "Result2: %A" result2
