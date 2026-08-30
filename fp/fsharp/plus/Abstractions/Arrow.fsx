78f208fc34bb4142165764ed43517be36f943947
airmanxtw <airmanx@stust.edu.tw>
2026-08-30 14:14:29 +0800

    feat: 新增 Arrow 操作详解文檔，包含多種 Arrow 操作及其範例

--- fp/fsharp/plus/Abstractions/Arrow.fsx
+++ fp/fsharp/plus/Abstractions/Arrow.fsx
@@ -1,4 +1,5 @@
 #r "nuget:FSharpPlus"
+
 open FSharpPlus
 open FSharpPlus.Data
 open FSharpPlus.Lens
@@ -6,7 +7,17 @@ open FSharpPlus.Lens
 let f1 = fun x -> x + 1
 let f2 = fun x -> x * 2
 
+let a1 = arr f1
+let a2 = arr f2
+
+let a12 = a1 *** a2
+
+
 let r1 = first f1
 
 
-printfn "Arrow Result: %A" (r1 (1, 10))
+printfn "Arrow Result1: %A" (r1 (1, 10))
+
+printfn "Arrow Result2: %A" (a1 10)
+
+printfn "Arrow Result3: %A" (a12 (1, 10))
