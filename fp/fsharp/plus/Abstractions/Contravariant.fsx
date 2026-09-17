e8e5a7d7067523600acc35ea2c817607dc5629bc
airmanxtw <airmanx@stust.edu.tw>
2026-09-18 00:57:44 +0800

    feat: 新增 contramap 規則示範及輸出

--- fp/fsharp/plus/Abstractions/Contravariant.fsx
+++ fp/fsharp/plus/Abstractions/Contravariant.fsx
@@ -14,3 +14,17 @@ let result = contramap f1 (fun i -> i.ToString())
 let result2 = f1 >> fun i -> i.ToString()
 
 printfn "%A" (result "hello")
+
+let rule1 = contramap id (fun i -> i.ToString()) 3 = id "3"
+printfn "rule1:%A" rule1
+
+
+
+let rule2_1 =
+    let g = fun y -> y + 1
+    let z = fun x -> x * 2
+    (contramap g << contramap z) (fun i -> i.ToString())
+
+let rule2_2 = contramap ((fun x -> x * 2) << fun y -> y + 1) (fun i -> i.ToString())
+printfn "%A" (rule2_1 10)
+printfn "%A" (rule2_2 10)
