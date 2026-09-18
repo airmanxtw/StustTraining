9ecd3550c5f47e3fd4b46462b644382488cdeaee
airmanxtw <airmanx@stust.edu.tw>
2026-09-18 22:00:01 +0800

    fix: 修正 rule2 的格式化及註解

--- fp/fsharp/plus/Abstractions/Functor.fsx
+++ fp/fsharp/plus/Abstractions/Functor.fsx
@@ -30,8 +30,7 @@ printfn "Data2 Result3: %A" data2Result3
 let rule1 = (|>>) data2 id = id data2
 printfn "Rule1: %A" rule1
 
-let rule2 = (|>>) data2 ((fun x -> x * 2) << fun y -> y + 1) =
-                   ((fun d -> (|>>) d (fun x -> x * 2)) << fun d -> (|>>) d (fun y -> y + 1)) data2
+let rule2 =
+    (|>>) data2 ((fun x -> x * 2) << fun y -> y + 1) = ((<<|) (fun x -> x * 2) << (<<|) (fun y -> y + 1)) data2
+//((fun d -> (|>>) d (fun x -> x * 2)) << fun d -> (|>>) d (fun y -> y + 1)) data2
 printfn "Rule2: %A" rule2
-
-
