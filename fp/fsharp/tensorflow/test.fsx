ce95605132dc48df22ab06220dbf4b1d3c81e8df
airmanxtw <airmanx@stust.edu.tw>
2026-09-20 00:26:39 +0800

    feat: 新增 TensorFlow 常數示範及輸出

--- fp/fsharp/tensorflow/test.fsx
+++ fp/fsharp/tensorflow/test.fsx
@@ -2,6 +2,7 @@
 #r "nuget:SciSharp.TensorFlow.Redist, 2.10.0"
 
 open Tensorflow
+
 open Tensorflow.NumPy
 
 let hello = Binding.tf.constant "Hello, TensorFlow.NET!"
@@ -10,5 +11,9 @@ printfn "%A" hello
 let t1 = new Tensor 3
 let t2 = new Tensor(new NDArray [| 1; 2; 3 |])
 
+let t3 = array2D [ [ 4; 5; 6 ]; [ 7; 8; 9 ] ] |> Binding.tf.constant
+
+
 printfn "%A" t1
 printfn "%A" t2
+printfn "%A" t3
