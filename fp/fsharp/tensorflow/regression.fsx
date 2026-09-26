d3271279eb6bfacb2fb6e72b77c29e81adf751db
airmanxtw <airmanx@stust.edu.tw>
2026-09-27 00:17:03 +0800

    feat: 新增回歸分析示例，包含訓練數據集

---
+++ fp/fsharp/tensorflow/regression.fsx
@@ -0,0 +1,14 @@
+#r "nuget:TensorFlow.NET, 0.100.4"
+#r "nuget:SciSharp.TensorFlow.Redist, 2.10.0"
+
+open Tensorflow
+
+open Tensorflow.NumPy
+
+let train_X = np.array [| 3.3; 4.4; 5.5; 6.71; 6.93; 4.168; 9.779; 6.182; 7.59; 2.167; 7.042; 10.791; 5.313; 7.997; 5.654; 9.27; 3.1 |]
+
+let train_Y = np.array [| 1.7; 2.76; 2.09; 3.19; 1.694; 1.573; 3.366; 2.596; 2.53; 1.221; 2.827; 3.465; 1.65; 2.904; 2.42; 2.94; 1.3 |]
+
+//let n_samples = train_X.shape.[0]
+
+//printfn "Number of samples: %d" n_samples
