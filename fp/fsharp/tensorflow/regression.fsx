56ea9b7e70204471b8118b938d0169a765be397e
airmanxtw <airmanx@stust.edu.tw>
2026-09-27 00:55:39 +0800

    feat: 更新回歸分析示例，使用浮點數陣列並新增訓練步驟及權重更新邏輯

--- fp/fsharp/tensorflow/regression.fsx
+++ fp/fsharp/tensorflow/regression.fsx
@@ -1,14 +1,79 @@
 #r "nuget:TensorFlow.NET, 0.100.4"
 #r "nuget:SciSharp.TensorFlow.Redist, 2.10.0"
+#r "nuget:TensorFlow.Keras, 0.10.4"
 
 open Tensorflow
 
 open Tensorflow.NumPy
 
-let train_X = np.array [| 3.3; 4.4; 5.5; 6.71; 6.93; 4.168; 9.779; 6.182; 7.59; 2.167; 7.042; 10.791; 5.313; 7.997; 5.654; 9.27; 3.1 |]
+let train_X =
+    np.array
+        [| 3.3f
+           4.4f
+           5.5f
+           6.71f
+           6.93f
+           4.168f
+           9.779f
+           6.182f
+           7.59f
+           2.167f
+           7.042f
+           10.791f
+           5.313f
+           7.997f
+           5.654f
+           9.27f
+           3.1f |]
 
-let train_Y = np.array [| 1.7; 2.76; 2.09; 3.19; 1.694; 1.573; 3.366; 2.596; 2.53; 1.221; 2.827; 3.465; 1.65; 2.904; 2.42; 2.94; 1.3 |]
+let train_Y =
+    np.array
+        [| 1.7f
+           2.76f
+           2.09f
+           3.19f
+           1.694f
+           1.573f
+           3.366f
+           2.596f
+           2.53f
+           1.221f
+           2.827f
+           3.465f
+           1.65f
+           2.904f
+           2.42f
+           2.94f
+           1.3f |]
 
-//let n_samples = train_X.shape.[0]
+let n_samples = train_X.shape.[0]
 
-//printfn "Number of samples: %d" n_samples
+let W = Binding.tf.Variable(0.0f, name = "weight")
+let b = Binding.tf.Variable(0.0f, name = "bias")
+
+let learning_rate = 0.01f
+let optimizer = KerasApi.keras.optimizers.SGD learning_rate
+
+let training_steps = 4000
+let display_step = 50
+
+for step in 1..training_steps do
+
+    use g = Binding.tf.GradientTape()
+    // Linear regression model
+    let pred = W * train_X + b
+
+    // Mean squared error
+    let loss = Binding.tf.reduce_mean (Binding.tf.square (pred - train_Y))
+
+    let gradients = g.gradient (loss, Seq.cast<IVariableV1> [ W; b ])
+
+    // Update weights
+    optimizer.apply_gradients (
+        Seq.zip gradients [| W; b |]
+        |> Seq.map (fun (gradient, variable) -> struct (Binding.tf.cast (gradient, TF_DataType.TF_FLOAT), variable))
+    )
+
+    if step % display_step = 0 then
+        Binding.print ("Step: ", step, " Loss: ", loss, " W: ", W, " b: ", b)
+//printfn "Step: %d, Loss: %O, W: %O, b: %O" step loss W b
